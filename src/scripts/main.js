2bd1a97c04bac98e77ae1bd884cbe7534e4d2870
Khnykin-Artem <k98030118@gmail.com>
2023-04-23 18:14:55 +0300

    fd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,9 @@
-import jump from './jump.js';
+import jump from './jump';
 import isAlive from './checkAlive.js';
 
 const dino = document.getElementById('dino');
 const cactus = document.getElementById('cactus');
 
-document.addEventListener(
-  'keydown',
-  (event) => event.code === 'Space' && jump(dino)
-);
+document.addEventListener('keydown', (event) => event.code === 'Space' && jump(dino));
 
 isAlive(dino, cactus);
