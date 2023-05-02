bbedcbec76c31df873964718611933a6671ac75d
Khnykin-Artem <k98030118@gmail.com>
2023-05-02 16:26:16 +0300

    create gulp task for fonts

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ import server from './gulp/tasks/server.js';
 import styles from './gulp/tasks/styles.js';
 import scripts from './gulp/tasks/scripts.js';
 import images from './gulp/tasks/images.js';
+import { otfToTtf, ttfToWoff, fontsStyle } from './gulp/tasks/fonts.js';
 
 global.app = {
   gulp,
@@ -21,7 +22,11 @@ function watcher() {
   gulp.watch(path.watch.images, images);
 }
 
-const mainTasks = gulp.parallel(html, styles, scripts, images);
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+const mainTasks = gulp.series(
+  fonts,
+  gulp.parallel(html, styles, scripts, images)
+);
 
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 gulp.task('default', dev);
